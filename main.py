66de1414f24d63e1c3312d090e3b6bb88887d52e
Daniel Eduardo Juárez Segura <eduardojuarez2602@gmail.com>
2022-05-03 09:12:42 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ print("Nombre: Daniel Eduardo Juarez Segura")
 print("Facultad: Ingenieria")
 print("Carrera: Informatica y Sistemas")
 print("Carne 1645122")
+
+input("Ingrese su animal favorito")
+animalUsuario=input()
+
+if animalUsuario == "perro" or animalUsuario == "Perro":
+    print("A mi tambien me gustan los perros")
+else:
+    print("Es un animal interesante, pero me gustan más los perros")
